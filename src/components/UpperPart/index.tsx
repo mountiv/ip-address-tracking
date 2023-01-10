6614b16c128e33191ec2e1fbae6c30a6de834eb9
SilvioMavrel <121834775+SilvioMavrel@users.noreply.github.com>
2023-01-10 22:10:04 +0100

    cahnged geolocation api, added intersection observer etc

--- src/components/UpperPart/index.tsx
+++ src/components/UpperPart/index.tsx
@@ -1,16 +1,44 @@
-import Parameters from "./Parameters";
-import Search from "./Search";
-import "./index.scss";
+import { useRef, useState, useMemo, useEffect } from "react";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
+import { useSelector } from "react-redux";
+import gif from "../../assets/loading.gif";
+import "./WorldMap.scss";
 
 // @ts-ignore
-const UpperPart = () => {
+function CustomMarker({ location }) {
+  const map = useMap();
+  if (location) map.flyTo(location, 12);
+
+  return location ? <Marker position={location}></Marker> : null;
+}
+
+// @ts-ignore
+const WorldMap = () => {
+  // @ts-ignore
+  const { lat, lon } = useSelector((state) => state.geo);
+
   return (
-    <section className="upper-part">
-      <h1>IP Address Tracker</h1>
-      <Search />
-      <Parameters />
-    </section>
+    <>
+      {lat ? (
+        <MapContainer
+          id="map"
+          center={[lat, lon]}
+          zoom={10}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <CustomMarker location={[lat, lon]} />
+        </MapContainer>
+      ) : (
+        <div className="map-loading">
+          <img src={gif} alt="" />
+        </div>
+      )}
+    </>
   );
 };
 
-export default UpperPart;
+export default WorldMap;
