8e53c230eca3aa6aeada078a242065a14254d2b2
SilvioMavrel <121834775+SilvioMavrel@users.noreply.github.com>
2023-01-10 04:04:38 +0100

    basic style

--- src/components/UpperPart/Search.tsx
+++ src/components/UpperPart/Search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./Search.scss";
 
 const Search = () => {
   return (
@@ -10,7 +10,7 @@ const Search = () => {
       />
       <button className="search-field__btn">
         <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-          <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+          <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
         </svg>
       </button>
     </div>
